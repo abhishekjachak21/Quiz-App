5f8498bc3cf876121bace6644c752a082235d8b3
Abhishek Jachak <abhishek-navnath.jachak@capgemini.com>
2025-02-12 20:55:10 +0530

    quiz service

---
+++ src/main/java/com/aj/quizapp/service/QuizService.java
@@ -0,0 +1,13 @@
+package com.aj.quizapp.service;
+
+import org.springframework.http.ResponseEntity;
+
+public class QuizService {
+	
+	QuizDao quizDao;
+
+	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+		return quiz;
+	}
+
+}
