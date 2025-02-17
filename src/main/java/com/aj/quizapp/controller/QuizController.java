13bc1ee53e3f6e3c2d6ae0f94671c41ff043a2fd
Abhishek Jachak <abhishek-navnath.jachak@capgemini.com>
2025-02-17 15:16:57 +0530

    controller of quiz update

--- src/main/java/com/aj/quizapp/controller/QuizController.java
+++ src/main/java/com/aj/quizapp/controller/QuizController.java
@@ -36,6 +36,7 @@ public class QuizController {
 		return quizService.getQuizQuestions(id);	
 	}
 	
+	@PostMapping("/submit/{id}")
 	public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response>responses){
 		return quizService.calculateResult(id, responses);
 	}
